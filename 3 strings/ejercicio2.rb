bac6cb5b1fcd957f8812481c9e2f12407dcfe829
Tomas <oyarzun.tomas@gmail.com>
2018-06-25 01:06:28 -0400

    Completed ejercicio2 and ejercicio3

--- 3 strings/ejercicio2.rb
+++ 3 strings/ejercicio2.rb
@@ -2,3 +2,26 @@
 # 1. Imprimir todos los elementos que excedan más de 5 caracteres.
 # 2. Crear un arreglo nuevo con todos los elementos en minúscula.
 # 3. Crear un método que devuelva un arreglo con la cantidad de caracteres que tiene cada nombre.
+
+
+
+# Construir un arreglo de los nombres de todos sus compañeros y en base a él:
+a = ['Tomás Oyarzún', 'Susana Zamora', 'Patricio Peña', 'Jaun Garcia', 'Jorge Hernández', 'Daniela Cortés', 
+	'Felipe Riveros', 'Sebastian Salinas', 'Vladimir Rendón', 'Felipe Castro', 'Leudis Jimenez', 'Gabriela Ilarreta',
+	'Marcelo Oñate', 'Daniela Venegas', 'Leonardo Bermundez', 'Damian Irarrazabal', 'Juan Hernandez',
+	'Frank Garrido', 'Maximiliano Américo']
+#1.-Imprimir todos los elementos que excedan más de 5 caracteres.
+a.each{|elem| puts elem if elem.length > 15}
+puts '--------------------'
+#2.-Crear un arreglo nuevo con todos los elementos en minúscula.
+b = []
+a.each_with_index{|elem, i| b[i] = elem.downcase}
+puts b
+puts '--------------------'
+#3.-Crear un método que devuelva un arreglo con la cantidad de caracteres que tiene cada nombre.
+def arraylength(array)
+	c = []
+	array.each_with_index{|elem, i| c[i] = elem.length}
+	return c
+end
+puts arraylength(a)
--- 3 strings/ejercicio3.rb
+++ 3 strings/ejercicio3.rb
@@ -6,3 +6,20 @@
 
 a = [1, 2, 3, 9, 12, 31, 'domingo']
 b = %w[lunes martes miércoles jueves viernes sábado domingo]
+
+# 1. La concatenación de a y b. (hint: Los elementos que se repiten en a y b pueden aparecer dos veces en el resultado).
+print a + b
+puts ''
+puts '---------------------------------------------------------------------------------------------------------'
+# 2. La unión de a y b. (hint: Los elementos que se repiten en a y b NO deben aparecer repetidos en el resultado).
+print a | b
+puts ''
+puts '---------------------------------------------------------------------------------------------------------'
+# 3. La intersección de a y b. (hint: El resultado debe estar compuesto por los elementos que se repiten en a y b).
+print a & b
+puts ''
+puts '---------------------------------------------------------------------------------------------------------'
+# 4. Intercalar los elementos: [[1, "lunes"], [2, "martes"], [3, "miércoles"], [9, "jueves"], [12, "viernes"], [31, "sábado"], ["domingo", "domingo"]]
+print a.zip(b) 
+puts ''
+
