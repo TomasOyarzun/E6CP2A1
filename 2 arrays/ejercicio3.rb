b63998e9c334b458f0e0f468ab5faf4e8f3793f2
Tomas <oyarzun.tomas@gmail.com>
2018-06-24 20:00:58 -0400

    Completed ejercicio3

--- 2 arrays/ejercicio3.rb
+++ 2 arrays/ejercicio3.rb
@@ -5,3 +5,38 @@
  # 4. Crear un método que incrementa todos los elementos en una unidad y devuelva un arreglo nuevo.
 
 a = [1,2,3,9,1,4,5,2,3,6,6]
+#Metodo para eliminar todos los números pares del arreglo.
+def removeifeven(array)
+	return array.each_with_index{|elem, i| array.delete(elem) if elem.even?}
+end
+#Metodo para sumar todos los elementos del arreglo.
+def sum(array)
+	storage = 0
+	array.each{|elem| storage += elem }
+	return storage
+end
+#Metodo para obtener el promedio de un arreglo llamando a otro metodo.
+def average(array)
+	average = 0
+	return average = (sum(array) / array.count.to_f)
+end
+#Metodo para incrementar los elementos en uno y regresar un nuevo arreglo.
+def incrementbyone(array)
+	b = []
+	array.each_with_index{|elem, i| b[i] = (elem + 1)}
+	return b 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
